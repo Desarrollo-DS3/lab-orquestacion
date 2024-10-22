31ab00e35556eee6b7fe79ef35eca4049546bf58
Nicolas Herrera <herreran903@gmail.com>
2024-10-22 17:15:25 -0500

    fix bugs

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,12 @@
-k3d cluster create mycluster -p "8082:30080@agent:0" --agents 3
+k3d cluster create mycluster -p "8082:30080@agent:0" --agents 6
 kubectl taint nodes k3d-mycluster-server-0 dedicated=server:NoSchedule
 kubectl label nodes k3d-mycluster-agent-0 gateway=true
-
 
 kubectl apply -f gateway/gateway-deployment.yaml
 kubectl apply -f samuel/samuel-deployment.yaml
 kubectl apply -f juan/juan-deployment.yaml
+kubectl apply -f john/john-deployment.yaml
+kubectl apply -f duque/duque-deployment.yaml
+kubectl apply -f julian/julian-deployment.yaml
 
 kubectl get pods
