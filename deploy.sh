d75c8822def560d532c1c4f5eec2f6209a4580bc
Nicolas Herrera <herreran903@gmail.com>
2024-10-22 21:46:25 -0500

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 k3d cluster create mycluster -p "8082:30080@agent:0" --agents 7
 kubectl taint nodes k3d-mycluster-server-0 dedicated=server:NoSchedule
-kubectl label nodes k3d-mycluster-agent-0 gateway=true
+kubectl taint nodes k3d-mycluster-agent-0 gateway=true:NoSchedule
 
 kubectl apply -f gateway/gateway-deployment.yaml
 kubectl apply -f samuel/samuel-deployment.yaml
@@ -9,5 +9,3 @@ kubectl apply -f john/john-deployment.yaml
 kubectl apply -f duque/duque-deployment.yaml
 kubectl apply -f julian/julian-deployment.yaml
 kubectl apply -f sebastian/sebastian-deployment.yaml
-
-kubectl get pods
