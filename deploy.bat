3840f0a01c37b629c106ef3b33c8399101dbec15
Nicolas Herrera <herreran903@gmail.com>
2024-10-22 21:16:58 -0500

    fix dockerhub name

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
-k3d cluster create mycluster -p "8082:30080@agent:0" --agents 6
+k3d cluster create mycluster -p "8082:30080@agent:0" --agents 7
 kubectl taint nodes k3d-mycluster-server-0 dedicated=server:NoSchedule
 kubectl label nodes k3d-mycluster-agent-0 gateway=true
 
@@ -8,5 +8,6 @@ kubectl apply -f juan/juan-deployment.yaml
 kubectl apply -f john/john-deployment.yaml
 kubectl apply -f duque/duque-deployment.yaml
 kubectl apply -f julian/julian-deployment.yaml
+kubectl apply -f sebastian/sebastian-deployment.yaml
 
 kubectl get pods
